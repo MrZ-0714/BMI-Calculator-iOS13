612e1556eb51c92770f4376c684a073e69d83112
MrZ-0714 <zimo.zhao.0714@gmail.com>
2021-04-08 16:01:53 -0700

    initial commit

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func heightSliderValueChanged(_ sender: UISlider) {
+        print(String(format:"%.2f", sender.value))
+        heightLabel.text = "\(String(format:"%.2f", sender.value)) m"
+        
+    }
+    @IBAction func weightSliderValueChanged(_ sender: UISlider) {
+        print(String(format:"%.2f", sender.value))
+        weightLabel.text = "\(String(format:"%.2f", sender.value)) kg"
+    }
 
 }
 
